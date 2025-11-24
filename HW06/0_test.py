f96c7ce768886a46c1b1e889f5b4ddfc3be0577b
Calum Heldt <cal.muon32@gmail.com>
2025-11-23 20:16:41 -0600

    done

--- HW06/0_test.py
+++ HW06/0_test.py
@@ -17,28 +17,41 @@ a = random.uniform(-1, 1)
 b = random.uniform(-1, 1)
 c = random.uniform(-1, 1)
 
-y1_0 = np.array([a, b, c])    # random initial value 1
-y2_0 = np.array([0.01, 0.01, 0.01])    # initial value 2
+y1_0 = np.array([a, b, c]); print(y1_0)     # random initial value 1
+y2_0 = np.array([0.001, 0.001, 0.001])         # initial value 2
 t0 = 0.0    # initial time
-T = 30      # final time
+T = 100      # final time
 dt = 0.01   # step size
 
 beta = 8/3; sigma = 10; rho = 20
+mu = 10**-5
+
 lorenz = lambda t , x : np.array([
     sigma*(x[1] - x[0]),
     x[0]*(rho - x[2]) - x[1],
     x[0]*x[1] - beta*x[2]
 ])
 
-mu = 1
-lorenz_modified = lambda t , x : np.array([
+lorenz_modifiedx = lambda t , x : np.array([
     sigma*(x[1] - x[0]) + mu*(lorenz(t, x)[0]-x[0]),
     x[0]*(rho - x[2]) - x[1],
     x[0]*x[1] - beta*x[2]
 ])
 
-X1 , X2, errors, t = rk4_modified(lorenz, lorenz_modified, y1_0, y2_0, t0, T, dt)
-# X , t = rk4_ndim(lorenz, y0, t0, T, dt)
+lorenz_modifiedy = lambda t , x : np.array([
+    sigma*(x[1] - x[0]) ,
+    x[0]*(rho - x[2]) - x[1] + mu*(lorenz(t, x)[1]-x[1]),
+    x[0]*x[1] - beta*x[2]
+])
+
+lorenz_modifiedz = lambda t , x : np.array([
+    sigma*(x[1] - x[0]) ,
+    x[0]*(rho - x[2]) - x[1],
+    x[0]*x[1] - beta*x[2] + mu*(lorenz(t, x)[2]-x[2]) 
+])
+
+X1 , X2, errors, t = rk4_modified(lorenz, lorenz_modifiedx, y1_0, y2_0, t0, T, dt)
+print(errors[-1])
 
 fig = plt.figure(figsize=(5,7)) # 1x2
 gs = GridSpec(2, 1, figure=fig) # actual layout
@@ -53,5 +66,16 @@ ax1.set_xlabel('x'); ax1.set_ylabel('y'); ax1.set_zlabel('z')
 ax2 = fig.add_subplot(gs[1, 0])
 ax2.semilogy(t, errors, color='red')
 ax2.set_xlabel('t'); ax2.set_ylabel('error(t)')
+
+
+"""
+# Check with original code for validation
+X , t = rk4_ndim(lorenz, y1_0, t0, T, dt)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection = '3d')
+ax.plot(X[0,:], X[1,:], X[2,:])
+ax.set_xlabel('x'); ax.set_ylabel('y'); ax.set_zlabel('z')
+"""
 
 plt.show()
