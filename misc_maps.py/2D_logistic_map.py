f955625cd277ba7af01653aa35b4cc8155127cf3
Calum Heldt <cal.muon32@gmail.com>
2025-11-29 19:24:44 -0600

    changed start, end for testing

--- misc_maps.py/2D_logistic_map.py
+++ misc_maps.py/2D_logistic_map.py
@@ -4,8 +4,9 @@ import math
 
 # constants 
 a = 2.8
-b = 3
-N = 500 # number of iterations
+b = 4
+N_start = 0 # first iteration to plot
+N_end = 10 # final iteration to plot
 
 # ICs in (0,1)
 x0 = 0.5
@@ -19,18 +20,21 @@ sin_list = [] # for color mapping comparison
 angles_list = [] # for color mapping comparison
 
 x=x0; y=y0
-for n in range(N):
-    x = a*x*(1-x)
-    y = b*y*(1-y)
+for n in range(N_end):
+    x = a*x*(1-y)
+    y = b*y*(1-x)
+    print(x)
+    print(y)
+    print("------")
 
-    if n > 100:
+    if n > N_start:
         x_list.append(x)
         y_list.append(y)
         count_list.append(n)
 
 # get sine map
 for n in count_list:
-    x = (n-100)*(2 * math.pi) / (N - 100) # 0 when n = 100, pi when n = 500
+    x = (n-N_start)*(2 * math.pi) / (N_end - N_start) # 0 when n = N_start, 2pi when n = N_end
     angles_list.append(x)
     sin_list.append(math.sin(x))
 
@@ -48,6 +52,6 @@ ax2 = fig.add_subplot(gs[1, 0])
 ax2.scatter(x_list, y_list, c=count_list, cmap='viridis')
 ax2.set_title("2D logistic map")
 
-fig.suptitle(f"Initial condition = ({x0},{y0}), a={a}, b={b}", fontsize=16, fontweight='bold') # got soln from google ai result
+fig.suptitle(f"Initial condition=({x0},{y0}), a={a}, b={b}", fontsize=16, fontweight='bold') # got soln from google search ai result
 
 plt.show()
